6214daa9c71634cf0e1f6cd1e288c0e1a7465b81
pofay <gianCarlo.gilos@outlook.com>
2018-10-20 11:27:06 +0800

    Added an Lcd display to show the current distance between sensor and target.

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-
 # Parking Sensor:
 
-The Code for the Parking Sensor I'm building for a CAPSTONE project.
+This Branch has an added [LCD][lcd] to display current distance from sensor to target
 
-* Used HC-SR04 For Distance Measurement 
-* Used pigpio as gpio library
+[Screenshot with lcd](/assets/screenshot_with_lcd.JPG)
+
+This Code is used for my Parking App by using a RPI with an [HC-SR04 Sensor][ultrasonic-sensor] to send whether a parking lot is vacant or occupied depending on the set `MAXIMUM_DISTANCE` in a `.env` file.
+
+There are two LEDs that signal whether (hypothetically) the lot is vacant or occupied.
+
+* Led at Pin 21 (Vacant)
+* Led at Pin 26 (Occupied)
+
+[Screenshot basic](/assets/screenshot_basic.JPG)
+
+Libraries used:
+
+* [Pigpio][pigpio] 
+* [Lcd][lcd]
+* [Axios][axios] for sending http requests to server
 
 
+[lcd]: https://github.com/fivdi/lcd
+[ultrasonic-sensor]: https://to-be-added
+[pigpio]: https://github.com/fivdi/pigpio
+[axios]: https://to-be-added
--- sensor.js
+++ sensor.js
@@ -1,7 +1,10 @@
 const Gpio = require('pigpio').Gpio
 const HttpClient = require('./http-client')
+const Lcd = require('lcd')
 const axios = require('axios')
 require('dotenv').config()
+
+const lcd = new Lcd({ rs: process.env.RS, e: process.env.E, data: [process.env.DB4, process.env.DB5, process.env.DB6, process.env.DB7], cols: 16, rows:2})
 
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
 const MICROSECONDS_PER_CM = 1e6/34321 
@@ -17,8 +20,11 @@ trigger.digitalWrite(0)  // Make sure trigger is low
 
 const client = new HttpClient(axios, process.env.LOT_ID)
 
+lcd.on('ready',() => {
+    lcd.print('Start Measuring')
+})
+
 const watchHCSR04 = () => {
-    console.log('Starting...')
     let startTick 
 
     echo.on('alert', (level, tick) => {
@@ -28,6 +34,7 @@ const watchHCSR04 = () => {
 	    const endTick = tick 
 	    const diff = (endTick >> 0) - (startTick >> 0)  // Unsigned 32 bit arithmetic
 	    const distance = diff / 2 / MICROSECONDS_PER_CM
+	    lcd.clear()
 	    if(distance <= process.env.MAXIMUM_DISTANCE) {
 		client.sendClosed()
 		ClosedLed.digitalWrite(1)
@@ -38,6 +45,11 @@ const watchHCSR04 = () => {
 		ClosedLed.digitalWrite(0)
 		OpenLed.digitalWrite(1)
 	    }
+	    lcd.setCursor(0,0)
+	    lcd.print('Dist. to target', (err) => {
+		lcd.setCursor(0,1)
+		lcd.print(`${distance.toFixed(3)} cm`)
+	    })
 	}
     }) 
 } 
